d31fdedac91f9afa33e831d3f5e7a0af175c750e
vanessaBonito <vanebonito@hotmail.com>
2022-11-15 14:26:50 -0500

    Update recipes and repice_foods routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   devise_for :users # Routes for authentication
  
   root "foods#index"  # Defines the root path route ("/")
-  resources :recipe_foods
+  # resources :recipe_foods
   resources :inventory_foods
   resources :inventories
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods
+    get '/add_food/', to: 'recipe_foods#add_food'
+    # get '/shopping_list', to: 'recipes#shopping_list'
+  end
   resources :foods
   resources :users
-  
+  get '/public_recipes', to: 'recipes#public_recipes'
   
 end

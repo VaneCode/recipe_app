7952587d81d03918221f6f40543a01ba384d4c74
vanessaBonito <vanebonito@hotmail.com>
2022-11-15 09:54:32 -0500

    Update model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  # Asociations
+  # Associations
   has_many :inventories, dependent: :destroy
   has_many :recipes, dependent: :destroy
 end

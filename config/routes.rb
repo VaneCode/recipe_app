c811209f759da7829a5c35edce2b0fd7d520404d
vanessaBonito <vanebonito@hotmail.com>
2022-11-14 21:47:00 -0500

    Create files for entities in the DB schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :recipe_foods
+  resources :inventory_foods
+  resources :inventories
+  resources :recipes
+  resources :foods
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users # Routes for authentication
+  # devise_for :users # Routes for authentication
  
-  root "foods#index"  # Defines the root path route ("/")
+  # root "foods#index"  # Defines the root path route ("/")
 end

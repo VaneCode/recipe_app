009182d45550b662a1a3bf7e876a2098a9611d84
wayungi <fwayungi@gmail.com>
2022-11-16 19:28:24 +0300

    DELETES: style sheet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+francis = User.create(name: 'francis' email: 'francis@gmail.com', password:'123456')
+obella = User.create(name: 'obella', email: 'obella@gmail.com', password:'123456')
+kibuuka = User.create(name: 'kibuuka', email: 'kibuuka@gmail.com', password:'123456')
+mahooro = User.create(name: 'mahooro', email: 'mahooro@gmail.com', password:'123456')
+anzoyo = User.create(name: 'anzoyo', email: 'anzoyo@gmail.com', password:'123456')
+sali = User.create(name: 'sali', email: 'sali@gmail.com', password:'123456')
+
+chogm = Inventory.create(name:'chogm', description: 'pork joint', user: francis)

f936c2217b3fbd55514fb5e045875cf8d3ca339d
Jean Bulambo <jeanbulambo4@gmail.com>
2022-11-16 22:15:58 +0200

    Add food component

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,8 @@ class Food < ApplicationRecord
   has_many :recipes, through: :recipe_foods
   has_many :inventory_foods, dependent: :destroy
   has_many :inventories, through: :recipe_foods
+
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :measurement_unit, presence: true
 end

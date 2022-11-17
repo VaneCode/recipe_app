943ced983b87dd2b834a620c639a5b5c5ed22298
wayungi <fwayungi@gmail.com>
2022-11-17 21:31:19 +0300

    ADDS: remove duplicate migrations

--- db/migrate/20221115212843_remove_columns_quantity_wrong_type.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsQuantityWrongType < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :inventory_foods, :quantity
-    remove_column :recipe_foods, :quantity
-  end
-end
--- db/migrate/20221115213146_add_quantity_to_recipe_foods.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityToRecipeFoods < ActiveRecord::Migration[7.0]
-  def change
-    add_column :recipe_foods, :quantity, :integer
-  end
-end
